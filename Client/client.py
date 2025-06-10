698df71ecb25af418b6f51cc585626accaa5dc8a
nicolas-beames <nicolas.beames04@gmai.com>
2025-06-10 18:12:09 -0300

    feat: List Srv Items

--- Client/client.py
+++ Client/client.py
@@ -3,13 +3,14 @@ import socket
 
 while True:
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    msgClient = str(input('digite o nome do arquivo que deseja receber: '))
 
-    client.sendto(msgClient.encode(), ('localhost', 55555))
+    print("1- Listar")
+    print("2- Baixar do Servidor")
+    opt = int(input('digite um numero: '))
 
-    with open('copia_' + msgClient, 'wb') as file:
-        while True:
+    match opt:
+        case 1:
+            client.sendto("LIST".encode(), ('localhost', 55555))
             msgReceivedBytes, addressServer = client.recvfrom(10000000)
 
             if msgReceivedBytes == b'EOF':
@@ -20,6 +21,28 @@ while True:
                 break
 
             print(f'dado recebido {msgReceivedBytes}')
-            file.write(msgReceivedBytes)
+            break
+        case 2:
+            msgClient = str(input("Qual arquivo gostaria de baixar? (arquivo.extensao): "))
+            client.sendto(msgClient.encode(), ('localhost', 55555))
+            with open('copia_' + msgClient, 'wb') as file:
+                while True:
+                    msgReceivedBytes, addressServer = client.recvfrom(10000000)
 
-    print('arquivo recebido')
+                    if msgReceivedBytes == b'EOF':
+                        break
+
+                    if msgReceivedBytes.startswith(b'ERRO'):
+                        print(msgReceivedBytes.decode())
+                        break
+
+                    print(f'dado recebido {msgReceivedBytes}')
+                    file.write(msgReceivedBytes)
+            escolha = str(input('Arquivo recebido com sucesso, gostaria de baixar mais algum? (s/n)'))
+
+            if escolha == "s" or escolha == "S":
+                print("WIP!")
+                break
+            elif escolha == "n" or escolha == "N":
+                print("nao escolhido")
+                break
--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,9 @@
 import socket
+import os
+
+
+#Diretorio base onde fica os arquivos
+basedir = 'arquivos/'
 
 #cria a conexão utilizando o protocolo UDP
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,14 +14,26 @@ while True:
     msgClientBytes, addressClient = server.recvfrom(1024) #recebe a mensagem do cliente e armazena tanto a mensagem como também o endereço
     msgClientString = msgClientBytes.decode() #decotifica a mensagem para string
 
-    print(f'arquivo escolhido pelo usuario: {msgClientString}')
+    #print(f'arquivo escolhido pelo usuario: {msgClientString}')
 
-    try:
-        with open(msgClientString, 'rb') as file:
-            for data in file:
-                server.sendto(data, addressClient)
-            server.sendto(b'EOF', addressClient)
-            print('arquivo enviado')
-    except FileNotFoundError:
-        server.sendto(b'ERRO: arquivo nao encontrado', addressClient)
+    if msgClientString == "LIST":
+        Arquivos = []
+        with os.scandir(basedir) as arquivos:
+            for arquivo in arquivos:
+                Arquivos.append(str(arquivo))
 
+        if not Arquivos:
+            print("Sem arquivos na pasta")
+            server.sendto("Pasta Vazia...".encode(), addressClient)
+        else:
+                server.sendto(str(Arquivos).encode(), addressClient)
+                print(f"Enviado Lista de arquivos para: {addressClient}")
+    else:
+        try:
+            with open(f'arquivos/{msgClientString}', 'rb') as file:
+                for data in file:
+                    server.sendto(data, addressClient)
+                server.sendto(b'EOF', addressClient)
+                print('arquivo enviado')
+        except FileNotFoundError:
+            server.sendto(b'ERRO: arquivo nao encontrado', addressClient)
