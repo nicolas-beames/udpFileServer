bc35ff4781b52177e3b15c393cdae7a8a6ff7477
nicolas-beames <nicolas.beames04@gmai.com>
2025-06-11 09:26:53 -0300

    feat: List Srv Items GUI

--- PyQt/client.py
+++ PyQt/client.py
@@ -1,9 +1,13 @@
 import sys
+import socket
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PySide6.QtCore import QFile
 
 sys.path.append("../Ui")
 from ui_client import Ui_MainWindow
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip, porta = "localhost", 55555
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,6 +15,7 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.but_upload.clicked.connect(self.upload_clicked)
+        self.ui.but_listar.clicked.connect(self.listar_clicked)
 
     def upload_clicked(self):
         print("DEBUG: Upload Clicado")
@@ -22,8 +27,32 @@ class MainWindow(QMainWindow):
         if upload_selecionar.exec():
             filenames = upload_selecionar.selectedFiles()
             if filenames:
-                self.file_list.addItems([str(Path(filename)) for filename in filenames])
+                #self.file_list.addItems([str(Path(filename)) for filename in filenames])
+                pass
 
+    def listar_clicked(self):
+        print("Listar Clicado")
+        client.sendto("LIST".encode(), (ip, porta))
+        msgReceivedBytes, addressServer = client.recvfrom(1024)
+
+        if msgReceivedBytes.startswith(b'ERRO'):
+            print("Erro!")
+
+        # Tratativa para a lista dos arquivos recebida do Srv
+        lista = str(msgReceivedBytes).split("\'")
+        lista = list(lista)
+        for item in lista:
+            if item.endswith('"<DirEntry \\'):
+                lista.remove(item)
+        for item in lista:
+            if not item.endswith("\\"):
+                lista.remove(item)
+        lista.pop()
+
+        #print(f'final: {lista}')
+
+        for item in lista:
+            self.ui.SrvFiles.addItem(str(item[:-1]))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- Ui/ui_client.py
+++ Ui/ui_client.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designerVUYwjr.ui'
+## Form generated from reading UI file 'clientnUaEsn.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.1
 ##
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QListWidget, QListWidgetItem,
+    QMainWindow, QPushButton, QSizePolicy, QStatusBar,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -27,13 +28,28 @@ class Ui_MainWindow(object):
         MainWindow.setMaximumSize(QSize(980, 720))
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.but_upload = QPushButton(self.centralwidget)
+        self.verticalWidget = QWidget(self.centralwidget)
+        self.verticalWidget.setObjectName(u"verticalWidget")
+        self.verticalWidget.setGeometry(QRect(630, 530, 281, 141))
+        self.verticalLayout = QVBoxLayout(self.verticalWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.but_upload = QPushButton(self.verticalWidget)
         self.but_upload.setObjectName(u"but_upload")
-        self.but_upload.setGeometry(QRect(420, 370, 88, 34))
-        self.label_upload = QLabel(self.centralwidget)
-        self.label_upload.setObjectName(u"label_upload")
-        self.label_upload.setGeometry(QRect(390, 290, 151, 61))
-        self.label_upload.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout.addWidget(self.but_upload)
+
+        self.but_listar = QPushButton(self.verticalWidget)
+        self.but_listar.setObjectName(u"but_listar")
+
+        self.verticalLayout.addWidget(self.but_listar)
+
+        self.SrvFiles = QListWidget(self.centralwidget)
+        self.SrvFiles.setObjectName(u"SrvFiles")
+        self.SrvFiles.setGeometry(QRect(-5, 61, 561, 641))
+        self.lab_arquivos = QLabel(self.centralwidget)
+        self.lab_arquivos.setObjectName(u"lab_arquivos")
+        self.lab_arquivos.setGeometry(QRect(190, 20, 171, 31))
+        self.lab_arquivos.setAlignment(Qt.AlignmentFlag.AlignCenter)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -47,6 +63,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.but_upload.setText(QCoreApplication.translate("MainWindow", u"Upload", None))
-        self.label_upload.setText(QCoreApplication.translate("MainWindow", u"File Upload ", None))
+        self.but_listar.setText(QCoreApplication.translate("MainWindow", u"Listar Arquivos", None))
+        self.lab_arquivos.setText(QCoreApplication.translate("MainWindow", u"Arquivos do Servidor", None))
     # retranslateUi
 
