c6fecd35586ed2f3d472c304e6ca30fcee984451
Filipe <filipemzanin@gmail.com>
2025-06-11 01:10:40 -0300

    feat: update client and server

--- Client/client.py
+++ Client/client.py
@@ -1,16 +1,18 @@
 import socket
-
+import threading
+import json
 
 while True:
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    msgClient = str(input('digite o nome do arquivo que deseja receber: '))
 
-    client.sendto(msgClient.encode(), ('localhost', 55555))
+    print("1- Listar")
+    print("2- Baixar do Servidor")
+    opt = int(input('digite um numero: '))
 
-    with open('copia_' + msgClient, 'wb') as file:
-        while True:
-            msgReceivedBytes, addressServer = client.recvfrom(10000000)
+    match opt:
+        case 1:
+            client.sendto("LIST".encode(), ('localhost', 55555))
+            msgReceivedBytes, addressServer = client.recvfrom(1024)
 
             if msgReceivedBytes == b'EOF':
                 break
@@ -20,6 +22,37 @@ while True:
                 break
 
             print(f'dado recebido {msgReceivedBytes}')
-            file.write(msgReceivedBytes)
+            break
+        case 2:
+            msgClient = str(input("Qual arquivo gostaria de baixar? (arquivo.extensao): "))
+            client.sendto(msgClient.encode(), ('localhost', 55555))
 
-    print('arquivo recebido')
+            pacotesRecebidos = {}
+            with open('copia_' + msgClient, 'wb') as file:
+                while True:
+                    msgReceivedBytes, addressServer = client.recvfrom(1000000)
+
+                    if msgReceivedBytes == b'EOF':
+                        break
+
+                    if msgReceivedBytes.startswith(b'ERRO'):
+                        print(msgReceivedBytes.decode())
+                        break
+
+                    numero_pacote = int.from_bytes(msgReceivedBytes[:4], byteorder='big')
+                    dados_pacote = msgReceivedBytes[4:]
+
+                    print(f'Pacote #{numero_pacote} recebido com {len(dados_pacote)} bytes')
+
+                    pacotesRecebidos[numero_pacote] = dados_pacote
+
+                # Depois de receber todos os pacotes, escreve o arquivo na ordem correta
+                for i in range(len(pacotesRecebidos)):
+                    file.write(pacotesRecebidos[i])
+            escolha = str(input('Arquivo recebido com sucesso, gostaria de baixar mais algum? (s/n): '))
+
+            if escolha == "s" or escolha == "S":
+                pass
+            elif escolha == "n" or escolha == "N":
+                print("nao escolhido")
+                break
--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,8 @@
 import socket
+import os
+
+#Diretorio base onde fica os arquivos
+basedir = 'arquivos/'
 
 #cria a conexão utilizando o protocolo UDP
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,12 +15,49 @@ while True:
 
     print(f'arquivo escolhido pelo usuario: {msgClientString}')
 
-    try:
-        with open(msgClientString, 'rb') as file:
-            for data in file:
-                server.sendto(data, addressClient)
-            server.sendto(b'EOF', addressClient)
-            print('arquivo enviado')
-    except FileNotFoundError:
-        server.sendto(b'ERRO: arquivo nao encontrado', addressClient)
+    if msgClientString == "LIST":
+        Arquivos = []
+        with os.scandir(basedir) as arquivos:
+            for arquivo in arquivos:
+                Arquivos.append(str(arquivo))
 
+        if not Arquivos:
+            print("Sem arquivos na pasta")
+            server.sendto("Pasta Vazia...".encode(), addressClient)
+        else:
+                server.sendto(str(Arquivos).encode(), addressClient)
+                print(f"Enviado Lista de arquivos para: {addressClient}")
+    else:
+        try:
+            with open(f'arquivos/{msgClientString}', 'rb') as file:
+                data = file.read()
+                
+                print(f'Tamanho total dos dados: {len(data)} bytes')
+
+                tamanho_pacote = 1024
+                pacotes = []
+
+                #a cada loop, pega um pedaço do arquivo com tamanho definido
+                for i in range(0, len(data), tamanho_pacote):
+                    pacotes.append(data[i:i+tamanho_pacote])
+
+                print(f"Número de pacotes: {len(pacotes)}")
+
+                if pacotes:
+                    print(f"Tamanho do primeiro pacote: {len(pacotes[0])} bytes")
+                    numero_pacote = 0
+                    for item in pacotes:
+                        # Converte o número do pacote para 4 bytes
+                        seq_bytes = numero_pacote.to_bytes(4, byteorder='big')
+                        mensagem = seq_bytes + item  # concatena o número com o pacote
+                        server.sendto(mensagem, addressClient)
+                        numero_pacote += 1
+
+                    server.sendto(b'EOF', addressClient)
+                else:
+                    print("A lista de pacotes está vazia.")
+
+
+               
+        except FileNotFoundError:
+            server.sendto(b'ERRO: arquivo nao encontrado', addressClient)
