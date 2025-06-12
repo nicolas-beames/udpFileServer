4235a6fc361276e8e7c5db9d8d15556d92723410
nicolas-beames <nicolas.beames04@gmai.com>
2025-06-12 00:28:56 -0300

    feat: Download files from server via GUI

--- PyQt/client.py
+++ PyQt/client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PySide6.QtGui import QStandardItemModel
 from PySide6.QtCore import QFile, Qt
 
 sys.path.append("../Ui")
@@ -18,8 +19,7 @@ class MainWindow(QMainWindow):
         self.ui.but_upload.clicked.connect(self.upload_clicked)
         self.ui.but_listar.clicked.connect(self.listar_clicked)
         self.ui.but_baixar.clicked.connect(self.baixar_clicked)
-
-
+        
     def listar_clicked(self):
         print("Listar Clicado")
         client.sendto("LIST".encode(), (ip, porta))
@@ -31,17 +31,11 @@ class MainWindow(QMainWindow):
         # Tratativa para a lista dos arquivos recebida do Srv
         lista = str(msgReceivedBytes).split("\'")
         lista = list(lista)
-        print(f'antes tratativa: {lista}')
         for item in lista:
             if item.endswith('Entry \\') or not item.endswith("\\"):
-                print(f'rem {item}')
                 lista.remove(item)
         lista.pop()
         lista.pop(0)
-        # print(lista)
-        # for item in lista:
-        #     if not item.endswith("\\"):
-        #         lista.remove(item)
 
         # Verifica se o item ja está na lista
         # caso não estiver ele insere
@@ -63,8 +57,52 @@ class MainWindow(QMainWindow):
                 pass
 
     def baixar_clicked(self):
-        pass
+        msgClient = self.ui.SrvFiles.currentIndex().data()
 
+        print(msgClient)
+        client.sendto(msgClient.encode(), ('localhost', 55555))
+
+        pacotesRecebidos = {}
+        with open('copia_' + msgClient, 'wb') as file:
+            while True:
+                try:
+                    msgReceivedBytes, addressServer = client.recvfrom(2048)
+
+                    if msgReceivedBytes == b'EOF':
+                        print("Arquivo recebido com sucesso!")
+                        break
+
+                    if msgReceivedBytes.startswith(b'ERRO'):
+                        print(msgReceivedBytes.decode())
+                        break
+
+                    numero_pacote = int.from_bytes(msgReceivedBytes[:4], byteorder='big')
+                    dados_pacote = msgReceivedBytes[4:]
+
+                    print(f'Pacote #{numero_pacote} recebido com {len(dados_pacote)} bytes')
+
+                    pacotesRecebidos[numero_pacote] = dados_pacote
+
+                    ack = numero_pacote.to_bytes(4, byteorder='big')
+                    client.sendto(ack, addressServer)
+
+                    print(f"ACK N: {numero_pacote} enviado")
+
+                # Depois de receber todos os pacotes, escreve o arquivo na ordem correta
+                    for i in range(len(pacotesRecebidos)):
+                        file.write(pacotesRecebidos[i])
+
+                    dlg_sucesso = QMessageBox(self)
+                    dlg_sucesso.setWindowTitle("Sucesso!")
+                    dlg_sucesso.setText("Arquivo baixado com sucesso!\n Ele estara na pasta do script/exe")
+                    btn_ok = dlg_sucesso.exec()
+
+                    if btn_ok == QMessageBox.Ok:
+                        print("Operação concluida com exito! Finalizando...")
+                except:
+                    print("Erro!")
+                finally:
+                    break
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
